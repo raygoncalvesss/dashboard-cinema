cd2df3e17221b84d29472c2e30f0238cf0628f1f
rayssa <rayssagoncalves.tds2.senai@gmail.com>
2025-03-11 09:33:49 -0300

    feat: create sidebar component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/header";
+import Sidebar from "@/components/sidebar";
 
 export default function Home() {
  return (
   <div>
    <Header />
+
+   <main className={styles.dashboardContainer}>
+     <Sidebar />
+   </main>
   </div>
  )
 }
